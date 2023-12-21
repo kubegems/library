8ccfd378ccfe9b3b21ffee5dcf6448c252fc03b7
cnfatal <cnfatal@gmail.com>
2023-12-21 15:55:50 +0800

    Refactor TLS configuration and add dynamic certificate reloading

--- rest/listen/listen.go
+++ rest/listen/listen.go
@@ -18,14 +18,17 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
-	"io/ioutil"
+	"fmt"
 	"net"
 	"net/http"
+	"os"
 	"strings"
+	"sync"
 
+	"github.com/fsnotify/fsnotify"
+	"github.com/go-logr/logr"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
-	"kubegems.io/library/log"
 )
 
 func ServeHTTPContext(ctx context.Context, listen string, handler http.Handler) error {
@@ -33,12 +36,20 @@ func ServeHTTPContext(ctx context.Context, listen string, handler http.Handler) 
 }
 
 func ServeContext(ctx context.Context, listen string, handler http.Handler, cert, key string) error {
+	log := logr.FromContextOrDiscard(ctx)
 	s := http.Server{
 		Handler: handler,
 		Addr:    listen,
 		BaseContext: func(_ net.Listener) context.Context {
 			return ctx
 		},
+	}
+	tlsconfig, err := NewDynamicTLSConfig(ctx, cert, key)
+	if err != nil {
+		return err
+	}
+	if tlsconfig != nil {
+		s.TLSConfig = tlsconfig
 	}
 	go func() {
 		<-ctx.Done()
@@ -73,7 +84,7 @@ func TLSConfig(cafile, certfile, keyfile string) (*tls.Config, error) {
 	config := &tls.Config{ClientCAs: x509.NewCertPool()}
 	// ca
 	if cafile != "" {
-		capem, err := ioutil.ReadFile(cafile)
+		capem, err := os.ReadFile(cafile)
 		if err != nil {
 			return nil, err
 		}
@@ -86,4 +97,92 @@ func TLSConfig(cafile, certfile, keyfile string) (*tls.Config, error) {
 	}
 	config.Certificates = append(config.Certificates, certificate)
 	return config, nil
+}
+
+func NewDynamicTLSConfig(ctx context.Context, certfile, keyfile string) (*tls.Config, error) {
+	if certfile == "" || keyfile == "" {
+		return nil, nil
+	}
+	dyn := &DynamicCertificate{certFile: certfile, keyFile: keyfile}
+	if err := dyn.Reload(ctx); err != nil {
+		return nil, err
+	}
+	go dyn.Watch(ctx)
+	return &tls.Config{GetConfigForClient: dyn.GetConfigForClient}, nil
+}
+
+type DynamicCertificate struct {
+	certificate       tls.Certificate
+	certFile, keyFile string
+	mu                sync.Mutex
+}
+
+func (c *DynamicCertificate) GetConfigForClient(hello *tls.ClientHelloInfo) (*tls.Config, error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return &tls.Config{
+		Certificates: []tls.Certificate{c.certificate},
+	}, nil
+}
+
+func (c *DynamicCertificate) Watch(ctx context.Context) error {
+	log := logr.FromContextOrDiscard(ctx)
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		return fmt.Errorf("error creating fsnotify watcher: %v", err)
+	}
+	defer w.Close()
+
+	if err := w.Add(c.certFile); err != nil {
+		return fmt.Errorf("error adding watch for file %s: %v", c.certFile, err)
+	}
+	if err := w.Add(c.keyFile); err != nil {
+		return fmt.Errorf("error adding watch for file %s: %v", c.keyFile, err)
+	}
+	for {
+		select {
+		case e := <-w.Events:
+			if e.Has(fsnotify.Remove) && !e.Has(fsnotify.Rename) {
+				if err := w.Remove(e.Name); err != nil {
+					log.Info("Failed to remove file watch, it may have been deleted", "file", e.Name, "err", err)
+				}
+				if err := w.Add(e.Name); err != nil {
+					return fmt.Errorf("error adding watch for file %s: %v", e.Name, err)
+				}
+			}
+			// reload cert
+			if err := c.Reload(ctx); err != nil {
+				// if we fail to reload the cert, use the old one, wait for the next event
+				log.Error(err, "failed to reload cert", "cert", c.certFile, "key", c.keyFile)
+			}
+		case err := <-w.Errors:
+			return fmt.Errorf("received fsnotify error: %v", err)
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func (c *DynamicCertificate) Reload(ctx context.Context) error {
+	log := logr.FromContextOrDiscard(ctx)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	cert, err := os.ReadFile(c.certFile)
+	if err != nil {
+		return err
+	}
+	key, err := os.ReadFile(c.keyFile)
+	if err != nil {
+		return err
+	}
+	if len(cert) == 0 || len(key) == 0 {
+		return fmt.Errorf("missing content for serving cert")
+	}
+	certificate, err := tls.X509KeyPair(cert, key)
+	if err != nil {
+		return err
+	}
+	c.certificate = certificate
+	log.Info("(re)loaded a new cert/key pair", "cert", c.certFile, "key", c.keyFile)
+	return nil
 }
